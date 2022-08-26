9b88afbb3342cd434882ebfb7a9667a96c65d0f3
RiByryn <maryantonovichru@gmail.com>
2022-08-26 13:45:36 +0300

    wrong answer 10

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import sys
-text = sys.stdin.read()
+import re
 
-text = input()
-sents = text.replace("!", ".").replace("?", ".").replace("-", "").replace(",", " ").replace(":", " ").replace(";", " ").split(".")
+text = sys.stdin.read()
+# text = "This sentence iz correkt! -It Has,No mista;.Kes et oll.\nBut there are two BIG mistakes in this one!\nand here is one more."
+# text = "This is a message with thREE mistakes::: and four sentences!!! Hope i;:;:t helps"
+# text = "Usual sentence."
+sents = re.split("[!?.]", text)
+# print(sents)
 sents_without_spaces = []
 count = 0
-for i in range(len(sents)-1):
-    sents_without_spaces.append(sents[i].strip())
-for i in range(len(sents_without_spaces)):
-    if sents_without_spaces[i][0].islower():
+for sent in sents:
+    sents_without_spaces.append(sent.strip())
+# print(sents_without_spaces)
+for i, sent_without_spaces in enumerate(sents_without_spaces):
+    if len(sent_without_spaces) > 1 and sent_without_spaces[0].islower():
         count = count + 1
 words = []
 for sent_without_spaces in sents_without_spaces:
-    words.extend(sent_without_spaces.split(" "))
+    comp = re.compile("[A-Za-z]+")
+    words.extend(comp.findall(sent_without_spaces))
+# print(words)
 for word in words:
     for j in range(1, len(word)):
         if word[j].isupper():
             count = count + 1
 print(count)
+
+
+
 
