237ed42d596e58ff4302604055d4090ad1122c86
RiByryn <maryantonovichru@gmail.com>
2022-08-26 15:51:57 +0300

    runtime error 1

--- main.py
+++ main.py
@@ -1,29 +1,25 @@
 import sys
 import re
 
-text = sys.stdin.read()
-# text = "This sentence iz correkt! -It Has,No mista;.Kes et oll.\nBut there are two BIG mistakes in this one!\nand here is one more."
+# text = sys.stdin.read()
+text = "This sentence iz correkt! -It Has,No mista;.Kes et oll.\nBut there are two BIG mistakes in this one!\nand here is one more."
 # text = "This is a message with thREE mistakes::: and four sentences!!! Hope i;:;:t helps"
 # text = "Usual sentence."
+# text = ";a!b!c!d!"
 sents = re.split("[!?.]", text)
-# print(sents)
-sents_without_spaces = []
+
 count = 0
+
 for sent in sents:
-    sents_without_spaces.append(sent.strip())
-# print(sents_without_spaces)
-for i, sent_without_spaces in enumerate(sents_without_spaces):
-    if len(sent_without_spaces) > 1 and sent_without_spaces[0].islower():
-        count = count + 1
-words = []
-for sent_without_spaces in sents_without_spaces:
     comp = re.compile("[A-Za-z]+")
-    words.extend(comp.findall(sent_without_spaces))
-# print(words)
-for word in words:
-    for j in range(1, len(word)):
-        if word[j].isupper():
-            count = count + 1
+    words = []
+    if len(sent) > 0 and comp.findall(sent)[0].islower():
+        count = count + 1
+    words.extend(comp.findall(sent))
+    for word in words:
+        for j in range(1, len(word)):
+            if word[j].isupper():
+                count = count + 1
 print(count)
 
 
